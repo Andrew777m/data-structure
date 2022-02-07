4883a6d57eca688d1a131a367612f7d45e3a39f0
Mohamed Emad El-Dien Mahmoud <85120725+3omdawy160@users.noreply.github.com>
2022-02-07 19:00:01 +0200

    Create getIdAndFollowers.cpp

---
+++ getIdAndFollowers.cpp
@@ -0,0 +1,26 @@
+string getIdAndFollowers(string s)
+{
+	int counter=0;
+	string IdAndFollowers;
+	for(int i=0;i<s.length();i++)
+	{
+		if(s[i]=='<' && s[i+1]=='i' && s[i+2]=='d' && s[i+3]=='>')
+		{
+			IdAndFollowers.push_back(s[i+4]);
+			IdAndFollowers.push_back(32);
+			if(counter==0)
+			{
+				IdAndFollowers+=" -> ";
+				IdAndFollowers+="{ " ;
+				counter=1;
+			}
+		}
+		else if(s[i]=='<' && s[i+1] == '/' && s[i+2] == 'u' && s[i+3] == 's' && s[i+4] == 'e' && s[i+5] == 'r' && s[i+6]=='>')
+		{
+			counter=0;
+			IdAndFollowers+="}" ;
+			IdAndFollowers.push_back(10);
+		}
+	}
+	return IdAndFollowers;
+}
